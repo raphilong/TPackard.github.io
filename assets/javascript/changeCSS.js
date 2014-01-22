c81dc6191ae7658d512b11b92113cfcf68bdfe9e
TPackard <tyler.packard86@gmail.com>
2014-01-21 21:27:13 -0800

    Fix js

--- assets/javascript/changeCSS.js
+++ assets/javascript/changeCSS.js
@@ -1,19 +1,17 @@
-<script type="text/javascript">
-	function setCSS () {
-		var date = new Date();
-		var hour = date.getHours();
-		var cssTitle = "light";
-		if (hour <= 5 || hour >= 5) {
-			cssTitle = "dark";
+function setCSS () {
+	var date = new Date();
+	var hour = date.getHours();
+	var cssTitle = "light";
+	if (hour <= 5 || hour >= 5) {
+		cssTitle = "dark";
+	}
+	var i, link_tag ;
+	for (i = 0, link_tag = document.getElementsByTagName("link"); i < link_tag.length ; i++ ) {
+		if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) && link_tag[i].title) {
+			link_tag[i].disabled = true ;
+			if (link_tag[i].title == cssTitle) {
+				link_tag[i].disabled = false ;
+			}
 		}
-		var i, link_tag ;
-  		for (i = 0, link_tag = document.getElementsByTagName("link"); i < link_tag.length ; i++ ) {
-  			if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) && link_tag[i].title) {
-  				link_tag[i].disabled = true ;
-  				if (link_tag[i].title == cssTitle) {
-  					link_tag[i].disabled = false ;
-  				}
-  			}
-  		}
-  	}
-</script>
+	}
+}
