e5ba40bd650150eec4bb81493abab7d0f9963bb2
TPackard <tyler.packard86@gmail.com>
2014-01-22 22:20:33 -0800

    Directories Fixed

---
+++ javascript/setCSS.js
@@ -0,0 +1,10 @@
+var currentTime = new Date().getHours();
+if (0 <= currentTime && currentTime <= 5) {
+	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
+}
+if (5 < currentTime&&currentTime <= 5) {
+	document.write("<link rel='stylesheet' href='/css/light.css' type='text/css'>");
+}
+if (5 < currentTime&&currentTime < 23) {
+	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
+}
