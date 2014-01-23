e5b9ce354ee49bcf2a115a0a5b3c00a5f5e315c0
TPackard <tyler.packard86@gmail.com>
2014-01-23 14:30:44 -0800

    Fixed JS and CSS

--- javascript/setCSS.js
+++ javascript/setCSS.js
@@ -2,9 +2,9 @@ var currentTime = new Date().getHours();
 if (0 <= currentTime && currentTime <= 5) {
 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
 }
-if (5 < currentTime&&currentTime <= 5) {
+if (5 < currentTime&&currentTime <= 17) {
 	document.write("<link rel='stylesheet' href='/css/light.css' type='text/css'>");
 }
-if (5 < currentTime&&currentTime < 23) {
+if (17 < currentTime&&currentTime < 23) {
 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
 }
