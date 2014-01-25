3b9515acf55f37fe712736e9addf5b1cbfe82e69
TPackard <tyler.packard86@gmail.com>
2014-01-25 14:04:06 -0800

    Modified CSS and Added Grids

--- javascript/setCSS.js
+++ javascript/setCSS.js
@@ -1,20 +1,21 @@
-var currentTime = new Date().getHours();
-var minutes = new Date().getMinutes();
-minutes = minutes * 60 * 1000;
-var waitTime = 0;
-if (0 <= currentTime && currentTime <= 6) {
-	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
-	waitTime = (7 - currentTime) * 60 * 60 * 1000;
-}
-if (6 < currentTime&&currentTime <= 18) {
-	document.write("<link rel='stylesheet' href='/css/light.css' type='text/css'>");
-	waitTime = (19 - currentTime) * 60 * 60 * 1000;
-}
-if (18 < currentTime&&currentTime <= 24) {
-	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
-	waitTime = (24 - currentTime) * 60 * 60 * 1000;
-}
+// var currentTime = new Date().getHours();
+// var minutes = new Date().getMinutes();
+// minutes = minutes * 60 * 1000;
+// var waitTime = 0;
+// if (0 <= currentTime && currentTime <= 6) {
+// 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
+// 	waitTime = (7 - currentTime) * 60 * 60 * 1000;
+// }
+// if (6 < currentTime&&currentTime <= 18) {
+// 	document.write("<link rel='stylesheet' href='/css/light.css' type='text/css'>");
+// 	waitTime = (19 - currentTime) * 60 * 60 * 1000;
+// }
+// if (18 < currentTime&&currentTime <= 24) {
+// 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
+// 	waitTime = (24 - currentTime) * 60 * 60 * 1000;
+// }
 
-setTimeout(function(){
-   window.location.reload(1);
-}, waitTime - minutes);
+// setTimeout(function(){
+//    window.location.reload(1);
+// }, waitTime - minutes);
+document.write("<link rel='stylesheet' href='/css/light.css' type='text/css'>");
