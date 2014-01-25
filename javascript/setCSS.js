933c2730317af9938f4fa55839ea1baeb5cab7a9
TPackard <tyler.packard86@gmail.com>
2014-01-24 23:03:15 -0800

    Auto reload added

--- javascript/setCSS.js
+++ javascript/setCSS.js
@@ -1,10 +1,18 @@
 var currentTime = new Date().getHours();
+var waitTime = 0;
 if (0 <= currentTime && currentTime <= 5) {
 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
+	waitTime = (5 - currentTime) * 60 * 60 * 1000;
 }
 if (5 < currentTime&&currentTime <= 17) {
 	document.write("<link rel='stylesheet' href='/css/light.css' type='text/css'>");
+	waitTime = (12 - currentTime) * 60 * 60 * 1000;
 }
 if (17 < currentTime&&currentTime < 23) {
 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
+	waitTime = (6 - currentTime) * 60 * 60 * 1000;
 }
+console.log(waitTime);
+setTimeout(function(){
+   window.location.reload(1);
+}, waitTime);
