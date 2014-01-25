069470fc4aa1048dfb13787ac608a29fb438e1c8
TPackard <tyler.packard86@gmail.com>
2014-01-24 23:19:02 -0800

    Added minutes to auto load

--- javascript/setCSS.js
+++ javascript/setCSS.js
@@ -1,4 +1,6 @@
 var currentTime = new Date().getHours();
+var minutes = new Date().getMinutes();
+minutes = minutes * 60 * 1000;
 var waitTime = 0;
 if (0 <= currentTime && currentTime <= 6) {
 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
@@ -6,7 +8,7 @@ if (0 <= currentTime && currentTime <= 6) {
 }
 if (6 < currentTime&&currentTime <= 18) {
 	document.write("<link rel='stylesheet' href='/css/light.css' type='text/css'>");
-	waitTime = (18 - currentTime) * 60 * 60 * 1000;
+	waitTime = (19 - currentTime) * 60 * 60 * 1000;
 }
 if (18 < currentTime&&currentTime <= 24) {
 	document.write("<link rel='stylesheet' href='/css/dark.css' type='text/css'>");
@@ -15,4 +17,4 @@ if (18 < currentTime&&currentTime <= 24) {
 
 setTimeout(function(){
    window.location.reload(1);
-}, waitTime);
+}, waitTime - minutes);
