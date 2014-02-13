0acadc2d8862c56e164f44460912a32ea54ccc51
TPackard <tyler.packard86@gmail.com>
2014-02-13 11:22:48 -0800

    Added jumping and fixed JS

---
+++ javascript/entity.js
@@ -0,0 +1,124 @@
+/*ENTITY DECLARATION*/
+function entity(imgSrc, x, y, speed, width, height, numFrames, numAnim) {
+    this.image = new Image();
+    this.image.src = imgSrc;
+
+    this.x = x || 0;
+    this.y = y || 0;
+    this.speed = speed || 1;
+
+    this.width = width || this.image.width;
+    this.height = height || this.image.height;
+
+    this.numFrames = numFrames || 1;
+    this.numAnim = numAnim || 1;
+
+    this.currentFrame = 0;
+    this.facingRight = false;
+    this.moving = false;
+    this.jumping = false;
+    this.canJump = true;
+    this.jumpOffset = 0;
+    this.jumpPoint = 0;
+    this.gravity = true;
+    this.onGround = false;
+    this.prevY = this.y;
+    
+    this.nextFrame = nextFrame;
+    this.setFrame = setFrame;
+    this.getX = getX;
+    this.getY = getY;
+    this.move = move;
+    this.jump = jump;
+    this.checkOnGround = checkOnGround;
+    this.gravitate = gravitate;
+    this.draw = drawEntity;
+}
+
+/*ENTITY FUNCTIONS*/
+function nextFrame() {
+    this.currentFrame++;
+    if (this.currentFrame >= this.numFrames) this.currentFrame = 0;
+    return this.currentFrame;
+}
+
+function setFrame(frameNum) {
+    this.currentFrame = frameNum >= this.numFrames ? 0 : frameNum;
+}
+
+function getX() {
+    return Math.round(this.x);
+}
+
+function getY() {
+    return Math.round(this.y);
+}
+
+function move(direction, delta) {
+    if (direction == up && this.canJump) {
+        this.jumping = true;
+        this.canJump = false;
+    }
+
+    if (direction == right) {
+        if (this.x < canvas.width - this.width) {
+            this.x += this.speed * delta;
+            this.moving = true;
+        }
+        this.facingRight = true;
+    }
+
+    if (direction == left) {
+        if (this.x > 0) {
+            this.x -= this.speed * delta;
+            this.moving = true;
+        }
+        this.facingRight = false;
+    }
+}
+
+function jump(delta) {
+    this.jumpPoint += delta * 25;
+    if (this.y > 0 && this.jumpPoint <= 10 && this.jumping) {
+        this.y += this.jumpOffset;
+        this.jumpOffset = -(this.jumpPoint - 20) * this.jumpPoint;
+        this.gravity = false;
+    } else {
+        this.jumpPoint = 0;
+        this.jumpOffset = 0;
+        this.jumping = false;
+        this.gravity = true;
+    }
+    this.y -= this.jumpOffset;
+}
+
+function checkOnGround(platforms) {
+    // FIXME Entity sometimes falls through the platform
+    var finished = false
+    this.onGround = false;
+    for (var platform of platforms) {
+        if (!finished) {
+            if (this.getY() + this.height >= platform.y * platform.tileSize && this.prevY + this.height <= platform.y * platform.tileSize && this.x + this.width >= platform.x * platform.tileSize && this.x <= (platform.x + platform.width) * platform.tileSize) {
+                this.onGround = true;
+                finished = true;
+                this.y = platform.y * platform.tileSize;
+                this.jumping = false;
+            }
+        }
+    }
+    this.prevY = this.getY();
+}
+
+function gravitate(delta) {
+    console.log(this.onGround +"\n");
+    if (this.getY() < canvas.height - this.height && this.gravity && !this.onGround) this.y += 200 * delta;
+    if (this.getY() >= canvas.height - this.height) {
+        this.y = canvas.height - this.height;
+        this.canJump = true;
+    }
+}
+
+function drawEntity(context) {
+    var offset = this.facingRight ? this.height + 1 : 0;
+    context.drawImage(this.image, (this.currentFrame * this.width + this.currentFrame), offset, this.width, this.height, this.getX(), this.getY(), this.width, this.height);
+}
--- javascript/game.js
+++ javascript/game.js
@@ -1,75 +1,12 @@
 /*CANVAS*/
 var canvas = document.getElementById("canvas");
 canvas.width = 800;
-canvas.height = 500;
+canvas.height = 496;
 var ctx = canvas.getContext("2d");
-
-/*ENTITY DECLARATION*/
-function entity(imgSrc, x, y, speed, width, height, numFrames, numAnim) {
-    this.image = new Image();
-    this.image.src = imgSrc;
-
-    this.x = x || 0;
-    this.y = y || 0;
-    this.speed = speed || 1;
-
-    this.width = width || this.image.width;
-    this.height = height || this.image.height;
-
-    this.numFrames = numFrames || 1;
-    this.numAnim = numAnim || 1;
-
-    this.currentFrame = 0;
-    this.facingRight = false;
-    this.moving = false;
-}
-
-/*ENTITY FUNCTIONS*/
-entity.prototype.nextFrame = function () {
-    this.currentFrame++;
-    if (this.currentFrame >= this.numFrames) this.currentFrame = 0;
-    return this.currentFrame;
-};
-
-entity.prototype.getX = function() {
-    return Math.round(this.x);
-};
-
-entity.prototype.getY = function() {
-    return Math.round(this.y);
-};
-
-entity.prototype.move = function(direction, delta) {
-    if (direction == up) {
-        this.y -= this.speed * delta;
-        this.moving = true;
-    }
-    
-    if (direction == down) {
-        this.y += this.speed * delta;
-        this.moving = true;
-    }
-    
-    if (direction == right) {
-        this.x += this.speed * delta;
-        this.moving = true;
-        this.facingRight = true;
-    }
-    
-    if (direction == left) {
-        this.x -= this.speed * delta;
-        this.moving = true;
-        this.facingRight = false;
-    }
-};
-
-entity.prototype.draw = function(context) {
-    var offset = this.facingRight ? this.height + 1 : 0;
-    context.drawImage(this.image, (this.currentFrame * this.width + this.currentFrame), offset, this.width, this.height, this.getX(), this.getY(), this.width, this.height);
-};
 
 /*GAME FUNCTIONS AND OBJECTS*/
 var person = new entity("Person.png", 400, 250, 200, 13, 32, 6);
+platforms.push(new platform(30, 30, 4, 1));
 
 var keysDown = {};
 var right = "right";
@@ -77,16 +14,19 @@ var left = "left";
 var up = "up";
 var down = "down";
 
-document.onkeydown = function(e) {
+document.onkeydown = function (e) {
     keysDown[e.keyCode] = true;
+    if (e.keyCode == 32 || e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) e.preventDefault();
 };
 
-document.onkeyup = function(e) {
+document.onkeyup = function (e) {
     delete keysDown[e.keyCode];
 };
 
-var update = function (delta) {
+function update(delta) {
     person.moving = false;
+    person.checkOnGround(platforms);
+    person.gravitate(delta);
     if (38 in keysDown) {
         person.move(up, delta);
     }
@@ -99,15 +39,17 @@ var update = function (delta) {
     if (39 in keysDown) {
         person.move(right, delta);
     }
+    person.jump(delta);
 };
 
-var paint = function () {
+function paint() {
     ctx.fillStyle = "#EEEEEE";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (var platform of platforms) platform.draw(ctx);
     person.draw(ctx);
 };
 
-var main = function () {
+function main() {
     var now = Date.now();
     var delta = now - then;
 
@@ -115,14 +57,15 @@ var main = function () {
     paint();
 
     if (now - lastSwitch >= 64) { // 16 times per second
-        person.currentFrame = person.moving ? person.nextFrame() : 0;
+        person.moving ? person.nextFrame() : person.setFrame(0);
         lastSwitch = now;
     }
 
     then = now;
+    setTimeout(main, 1);
 };
 
 /*START GAME*/
 var lastSwitch = Date.now();
 var then = Date.now();
-setInterval(main, 1);
+main();
